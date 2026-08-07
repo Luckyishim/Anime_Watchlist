36df9c4ad6a70d00ec59811b8e3b9eb260d1281a
unknown <luckyrajkarnikar@gmail.com>
2026-08-07 23:22:11 +0545

    Pagination Learned

--- server_side/controllers/animeController.js
+++ server_side/controllers/animeController.js
@@ -5,10 +5,16 @@ import Anime from "../models/Anime.js"
 
 //To get all Anime
 export const getAllAnime = async (req, res) => {
-    const anime = await Anime.find();
-    res.json(anime)
-}
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
 
+    const skip = (page - 1) * limit;
+
+    const anime = await Anime.find()
+    .skip(skip)
+    .limit(limit);
+res.json(anime)
+}
 
 //To get Anime by ID
 export const getAnimeById = async (req, res) => {
@@ -36,7 +42,7 @@ export const deleteAnime = async (req, res) => {
 }
 
 //To delete all
-export const deleteAllAnime = async(req,res)=>{
+export const deleteAllAnime = async (req, res) => {
     const anime = await Anime.deleteMany({})
     res.json(anime)
 }
@@ -49,8 +55,8 @@ export const getActionAnime = async (req, res) => {
 }
 
 //To get Anime based on status
-export const getAnimeStatus = async(req, res)=>{
-    const status = req.query.status ? {status: req.query.status}: {}
+export const getAnimeStatus = async (req, res) => {
+    const status = req.query.status ? { status: req.query.status } : {}
     const anime = await Anime.find(status)
     res.json(anime)
 }
