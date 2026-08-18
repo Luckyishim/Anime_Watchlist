821ab1f6ef37016d5fa7ba14b3feca679fcb1eb7
unknown <luckyrajkarnikar@gmail.com>
2026-08-18 19:29:42 +0545

    Static Card Created

--- client_side/Watchlist/src/components/Card.jsx
+++ client_side/Watchlist/src/components/Card.jsx
@@ -2,13 +2,34 @@ import "../styles/Card.css"
 export default function Card() {
   return (
     <div className="card">
-      <div>
+      <div className="status">
         <h2>One Piece</h2>
         <p>Completed</p>
       </div>
-      <div>
-        <span>Action</span>
-        <span>Adventure</span>
+      <div className="genre">
+        <p>Action</p>
+        <p>Adventure</p>
+        <p>Fantasy</p>
+      </div>
+      <div className="details">
+        <div className="episodes">
+          <p>Episodes</p>
+          <p>44 / 44</p>
+          <p>Season 3</p>
+        </div>
+        <span className="line">
+
+        </span>
+        <div className="rating">
+          <p>Rating</p>
+          <p>⭐8.7 / 10</p>
+          <p>Avg. User</p>
+        </div>
+      </div>
+
+      <div className="ctrl">
+        <button className="btn-edit">Edit</button>
+        <button className="btn-detail">View Details</button>
       </div>
     </div>
   )
