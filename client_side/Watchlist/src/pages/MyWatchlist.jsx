c89a102f76a5564b703e98f35348c6cb5b9926a0
Luckyishim <luckyrajkarnikar@gmail.com>
2026-08-21 22:18:58 +0545

    simple start

--- client_side/Watchlist/src/pages/MyWatchlist.jsx
+++ client_side/Watchlist/src/pages/MyWatchlist.jsx
@@ -2,7 +2,17 @@ import Watchlist from './../components/Watchlist';
 export default function MyWatchlist() {
   return (
     <div>
+      <div className="title">
+        <h1>My Anime Watchlist</h1>
+        <p>Manage and Filter every anime in your collection</p>
+      </div>
+      <div className="stats">
+        <h3>
+          24
+        </h3>
+        <p>Total Anime</p>
 
+      </div>
       <div>
         {<Watchlist />}
       </div>
